02bb0697699693f9b9bbf8ca7d6370a8d8104448
Rafael Martinez do Carmo <rafaelmdocarmo@gmail.com>
2023-02-07 15:38:14 -0300

    OnInit e OnChanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <app-title title="Texto Alterado no App Component pelo Input"></app-title>
+  <router-outlet></router-outlet>`
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   public title = "Bem Vindo!" 
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log(1)
+    setTimeout(() => {console.log(2)}, 5000)
+
+  }
 
 }
--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent implements OnInit {
+export class TitleComponent implements OnInit, OnChanges{
 
-  public title: string = "Bem vindo Componente!"
+  @Input() public title: string = "Bem vindo Componente!"
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
+    console.log("Página foi ALterada com Input")
   }
 
 }
